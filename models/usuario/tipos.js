0ec7b3ed3f26d680eb7fcffd3b6c724583370091
Pedro-Sanchez09 <psanchezc09@gmail.com>
2021-12-08 09:32:24 -0500

    query usuarios no pendietes con operador $ne

--- models/usuario/tipos.js
+++ models/usuario/tipos.js
@@ -25,6 +25,7 @@ const tiposUsuario = gql`
   type Query {
     Usuarios(filtro: FiltroUsuarios): [Usuario]
     Usuario(_id: String!): Usuario
+    UsuariosNoPendiente(estado:String):[Usuario]
   }
 
   type Mutation {
