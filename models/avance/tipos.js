cc6b31a5bd1361cfe4c8aa4fb2ea154748e245dc
AndreaCTS <andreats0729@gmail.com>
2021-12-11 17:05:44 -0500

    agregar observacionesen crear avances

--- models/avance/tipos.js
+++ models/avance/tipos.js
@@ -15,7 +15,7 @@ const tiposAvance = gql`
     filtrarAvance(_id: String!): [Avance]
   }
   type Mutation {
-    crearAvance(fecha: Date!, descripcion: String!, proyecto: String!, creadoPor: String!): Avance
+    crearAvance(fecha: Date, descripcion: String!,observaciones: [String], proyecto: String!, creadoPor: String!): Avance
   
     editarAvance(
       _id: ID!
